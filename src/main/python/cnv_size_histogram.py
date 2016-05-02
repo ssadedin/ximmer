3689db5e23d3f66e1ed86fc2ce4b4a4e9ba5ebf4
Simon Sadedin <ssadedin@gmail.com>
2016-05-02 15:57:18 +1000

    better tick labels, bins and fix clipping of x-axis labels

--- src/main/python/cnv_size_histogram.py
+++ src/main/python/cnv_size_histogram.py
@@ -15,30 +15,31 @@ all_truth = pd.read_table(cnv_file)
 all_truth['span'] = np.log10(all_truth.end - all_truth.start)
 
 # Little bit of a hack: not sure why targeted bp is null for some cases?
-all_truth = all_truth[all_truth.tgbp != "null"]
-all_truth['tgbp'] = np.log10(pd.to_numeric(all_truth.tgbp))
 
+if len(all_truth.tgbp.values)>0 and type(all_truth.tgbp.values[0]) != np.int64:
+    all_truth = all_truth[all_truth.tgbp != "null"]
+    all_truth['tgbp'] = np.log10(pd.to_numeric(all_truth.tgbp))
 
 fig = plt.figure(figsize=(4,7))
 
 plt.subplot(311)
 plt.tick_params(axis='both', which='major', labelsize=7)
 dp = sns.distplot(all_truth.span, kde=False, bins=10)
-dp.set_title("Distribution of CNV Sizes")
 dp.set_xlabel(r"Spanned Base Pairs ($log_{10}(bp)$)")
+dp.set_title("Distribution of CNV Sizes")
 
 plt.subplot(312)
 plt.tick_params(axis='both', which='major', labelsize=7)
 dp = sns.distplot(all_truth.tgbp, kde=False, bins=20)
-# dp.set_title("Distribution of CNV Sizes")
-dp.set_xlabel(r"Targeted Base Pairs ($log_{10}(bp)$)")
+dp.set_xlabel(r"Targeted Base Pairs (bp)")
 
 plt.subplot(313)
 plt.tick_params(axis='both', which='major', labelsize=7)
-dp = sns.distplot(all_truth.tgbp, kde=False)
-# dp.set_title("Distribution of CNV Sizes")
+tg_bins = range(0,all_truth.targets.max())
+dp = sns.distplot(all_truth.targets, kde=False, bins=tg_bins)
 dp.set_xlabel(r"Target Regions")
-
-fig.subplots_adjust(hspace=0.1)
+dp.set_xticks([x + 0.5 for x in tg_bins])
+dp.set_xticklabels(tg_bins)
+fig.subplots_adjust(hspace=0.35)
 
 plt.savefig(output_file)
