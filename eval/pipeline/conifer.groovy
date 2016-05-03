2108e187521577947f29a706c33eaafbcf3de6c0
Simon Sadedin <ssadedin@gmail.com>
2016-05-03 17:33:13 +1000

    use CONIFER variable as full path to conifer.py instead of directory

--- eval/pipeline/conifer.groovy
+++ eval/pipeline/conifer.groovy
@@ -16,7 +16,7 @@ conifer_rpkm = {
     
     produce(rpkmOutput) {
         exec """
-        LD_LIBRARY_PATH=$HDF5_DIR/lib $PYTHON $CONIFER/conifer.py rpkm 
+        LD_LIBRARY_PATH=$HDF5_DIR/lib $PYTHON $CONIFER rpkm 
           --probes $target_bed
           --output $output.rpkm
           --input $input.bam
@@ -30,13 +30,12 @@ conifer_analyze = {
 
     produce(batch_name+".conifer.hdf5",batch_name+".scree.png") {
         exec """
-        echo "Conifer is analyzing $inputs.rpkm"
 
-        LD_LIBRARY_PATH=$HDF5_DIR/lib $PYTHON $CONIFER/conifer.py analyze 
+        LD_LIBRARY_PATH=$HDF5_DIR/lib $PYTHON $CONIFER analyze 
           --probes $target_bed
           --rpkm_dir ${file(input.rpkm).parentFile.absolutePath}
           --output $output.hdf5
-          --svd 3
+          --svd 1
           --write_svals singular_values.txt
           --write_sd sd_values.txt
           --plot_scree $output.png
@@ -48,7 +47,7 @@ conifer_call = {
     requires batch_name : "The name of the batch that Conifer is analysing"
     produce(batch_name+ ".conifer.cnvs.tsv") {
         exec """
-        LD_LIBRARY_PATH=$HDF5_DIR/lib $PYTHON $CONIFER/conifer.py call 
+        LD_LIBRARY_PATH=$HDF5_DIR/lib $PYTHON $CONIFER call 
         --input $input.hdf5 
         --output $output.tsv
         """
