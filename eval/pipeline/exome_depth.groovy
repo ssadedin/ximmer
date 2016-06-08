89f45fa158a4f3e313c396586fc864aa71467945
Simon Sadedin <ssadedin@gmail.com>
2016-06-08 12:14:58 +1000

    make exome depth output be tab separated - otherwise content with spaces misparses

--- eval/pipeline/exome_depth.groovy
+++ eval/pipeline/exome_depth.groovy
@@ -21,113 +21,112 @@ run_exome_depth = {
 
     println "Using Exome Depth transition probability = $transition_probability"
 
-    produce(batch_name + "." + chr + ".exome_depth.tsv") {
-	    R({"""
+    produce(batch)
+    R({"""
 
-		source("$TOOLS/r-utils/cnv_utils.R")
+        source("$TOOLS/r-utils/cnv_utils.R")
 
-		library(ExomeDepth)
+        library(ExomeDepth)
 
-		# Reference sequence
-		hg19.fasta = "$HGFA"
+        # Reference sequence
+        hg19.fasta = "$HGFA"
 
-		# Read the target / covered region
-		print(sprintf("Reading target regions for $chr from $target_bed"))
-		dsd.covered = read.bed(pipe("grep '$chr[^0-9]' $target_bed"))
+        # Read the target / covered region
+        print(sprintf("Reading target regions for $chr from $target_bed"))
+        dsd.covered = read.bed(pipe("grep '$chr[^0-9]' $target_bed"))
 
-		# ExomeDepth wants the columns named differently
-		dsd.covered = data.frame(
-		    chromosome=dsd.covered\$chr, 
-		    start=dsd.covered$start, 
-		    end=dsd.covered$end,  
-		    name=paste(dsd.covered\$chr,dsd.covered$start,dsd.covered\$end,sep="-")
-		)
+        # ExomeDepth wants the columns named differently
+        dsd.covered = data.frame(
+            chromosome=dsd.covered\$chr, 
+            start=dsd.covered$start, 
+            end=dsd.covered$end,  
+            name=paste(dsd.covered\$chr,dsd.covered$start,dsd.covered\$end,sep="-")
+        )
 
-		# Now we need all the bam files. Generate them from sample names
-		dsd.samples = c(${sample_list.collect{'"'+it+'"'}.join(",")})
+        # Now we need all the bam files. Generate them from sample names
+        dsd.samples = c(${sample_list.collect{'"'+it+'"'}.join(",")})
 
-		print(sprintf("Read %d samples",length(dsd.samples)))
+        print(sprintf("Read %d samples",length(dsd.samples)))
 
-		# Here we rely on ASSUMPTIONs:  - Single BAM file per sample
-		dsd.bam.files = c(${sample_info.collect { key, s -> "'$s.sample'='${s.files.bam[0]}'"}.join(",") })
+        # Here we rely on ASSUMPTIONs:  - Single BAM file per sample
+        dsd.bam.files = c(${sample_info.collect { key, s -> "'$s.sample'='${s.files.bam[0]}'"}.join(",") })
 
-		print(sprintf("Found %d bam files",length(dsd.bam.files)))
+        print(sprintf("Found %d bam files",length(dsd.bam.files)))
 
-		# Finally we can call ExomeDepth
-		dsd.counts <- getBamCounts(bed.frame = dsd.covered,
-					  bam.files = dsd.bam.files,
-					  include.chr = F,
-					  referenceFasta = hg19.fasta)
+        # Finally we can call ExomeDepth
+        dsd.counts <- getBamCounts(bed.frame = dsd.covered,
+                                  bam.files = dsd.bam.files,
+                                  include.chr = F,
+                                  referenceFasta = hg19.fasta)
 
-		# Note: at this point dsd.counts has column names reflecting the file names => convert to actual sample names
-		print(sprintf("Successfully counted reads in BAM files"))
+        # Note: at this point dsd.counts has column names reflecting the file names => convert to actual sample names
+        print(sprintf("Successfully counted reads in BAM files"))
 
-		colnames(dsd.counts) = c("GC", dsd.samples)
+        colnames(dsd.counts) = c("GC", dsd.samples)
 
-		# Problem: sample names starting with numbers get mangled. So convert them back, but ignore the first column
-		# which is actually the GC percentage
-		dsd.samples = colnames(dsd.counts)[-1]
+        # Problem: sample names starting with numbers get mangled. So convert them back, but ignore the first column
+        # which is actually the GC percentage
+        dsd.samples = colnames(dsd.counts)[-1]
 
-		write(paste("start.p","end.p","type","nexons","start","end","chromosome","id","BF","reads.expected","reads.observed","reads.ratio","sample",sep=" "), "$output.tsv")
+	write(paste("start.p","end.p","type","nexons","start","end","chromosome","id","BF","reads.expected","reads.observed","reads.ratio","sample",sep="\\t"), "$output.tsv")
 
-		for(dsd.test.sample in dsd.samples) {
+        for(dsd.test.sample in dsd.samples) {
 
-		    print(sprintf("Processing sample %s", dsd.test.sample))
+            print(sprintf("Processing sample %s", dsd.test.sample))
 
-		    dsd.reference.samples = dsd.samples[-match(dsd.test.sample, dsd.samples)]
+            dsd.reference.samples = dsd.samples[-match(dsd.test.sample, dsd.samples)]
 
-		    dsd.counts.df = as.data.frame(dsd.counts[,dsd.reference.samples])[,-1:-6]
+            dsd.counts.df = as.data.frame(dsd.counts[,dsd.reference.samples])[,-1:-6]
 
-		    dsd.test.sample.counts = dsd.counts[,dsd.test.sample][[1]]
+            dsd.test.sample.counts = dsd.counts[,dsd.test.sample][[1]]
 
-		    print(sprintf("Selecting reference set for %s ...", dsd.test.sample ))
-		    dsd.reference = select.reference.set(
-					     test.counts = dsd.counts[,dsd.test.sample][[1]],
-					     reference.counts = as.matrix(as.data.frame(dsd.counts[,dsd.reference.samples])[,-1:-6]),
-					     bin.length = dsd.covered$end - dsd.covered$start
-					    )
+            print(sprintf("Selecting reference set for %s ...", dsd.test.sample ))
+            dsd.reference = select.reference.set(
+                                     test.counts = dsd.counts[,dsd.test.sample][[1]],
+                                     reference.counts = as.matrix(as.data.frame(dsd.counts[,dsd.reference.samples])[,-1:-6]),
+                                     bin.length = dsd.covered$end - dsd.covered$start
+                                    )
 
-		    # Get counts just for the reference set
-		    dsd.reference.counts = apply(dsd.counts.df[,dsd.reference\$reference.choice,drop=F],1,sum)
+            # Get counts just for the reference set
+            dsd.reference.counts = apply(dsd.counts.df[,dsd.reference\$reference.choice,drop=F],1,sum)
 
-		    print(sprintf("Creating ExomeDepth object ..."))
-		    dsd.ed = new("ExomeDepth",
-				  test = dsd.test.sample.counts,
-				  reference = dsd.reference.counts,
-				  formula = "cbind(test, reference) ~ 1")
-
-
-		    print(sprintf("Calling CNVs ..."))
-		    dsd.cnvs = CallCNVs(x = dsd.ed,
-					    transition.probability = $transition_probability,
-					    chromosome = dsd.covered$chromosome,
-					    start = dsd.covered$start,
-					    end = dsd.covered$end,
-					    name = dsd.covered$name)
+            print(sprintf("Creating ExomeDepth object ..."))
+            dsd.ed = new("ExomeDepth",
+                          test = dsd.test.sample.counts,
+                          reference = dsd.reference.counts,
+                          formula = "cbind(test, reference) ~ 1")
 
 
-		    dsd.results = dsd.cnvs@CNV.calls
-		    dsd.results$sample = rep(dsd.test.sample, nrow(dsd.results))
+            print(sprintf("Calling CNVs ..."))
+            dsd.cnvs = CallCNVs(x = dsd.ed,
+                                    transition.probability = $transition_probability,
+                                    chromosome = dsd.covered$chromosome,
+                                    start = dsd.covered$start,
+                                    end = dsd.covered$end,
+                                    name = dsd.covered$name)
 
-		    print(sprintf("Writing results ..."))
-		    if(nrow(dsd.results)>0) {
-			write.table(file="$output.tsv", 
-				    x=dsd.results,
-				    row.names=F,
-				    col.names=F,
-				    append=T)
-		    } 
-		}
 
-		print(sprintf("Finished"))
+            dsd.results = dsd.cnvs@CNV.calls
+            dsd.results$sample = rep(dsd.test.sample, nrow(dsd.results))
 
-	    """},'exome_depth')
-    }
+            print(sprintf("Writing results ..."))
+            if(nrow(dsd.results)>0) {
+                write.table(file="$output.tsv", 
+                            x=dsd.results,
+                            row.names=F,
+                            col.names=F,
+                            append=T)
+            } 
+        }
+
+        print(sprintf("Finished"))
+
+    """},'exome_depth')
 }
 
 merge_ed = {
     exec """
-        cat $inputs.exome_depth.tsv | grep -v '^sample' | awk '{ if(NR==1 || \$1 != "start.p") print \$0 }' > ${output(batch_name + ".exome_depth.cnvs.tsv")}
+        cat $inputs.exome_depth.tsv | grep -v '^"sample"' | awk '{ if(NR==1 || \$1 != "\\"start.p\\"") print \$0 }' > ${output(batch_name + ".exome_depth.cnvs.tsv")}
     """
 }
 
