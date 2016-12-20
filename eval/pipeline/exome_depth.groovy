dda793a6855cabdffbb99882b1d4fb5e12393764
Simon Sadedin <ssadedin@gmail.com>
2016-12-19 21:48:09 -0500

    fix wrong chromosomes included when run on GRCh37

--- eval/pipeline/exome_depth.groovy
+++ eval/pipeline/exome_depth.groovy
@@ -32,7 +32,7 @@ run_exome_depth = {
 
             # Read the target / covered region
             print(sprintf("Reading target regions for $chr from $target_bed"))
-            dsd.covered = read.bed(pipe("grep '$chr[^0-9]' $target_bed"))
+            dsd.covered = read.bed(pipe("grep '^$chr[^0-9]' $target_bed"))
 
             # ExomeDepth wants the columns named differently
             dsd.covered = data.frame(
