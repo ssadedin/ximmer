48239ae8ee195f2cf022e2501e36d21bcce3d518
Simon Sadedin <ssadedin@gmail.com>
2017-05-30 10:57:29 +1000

    update bpipe version to released 0.9.9.3 + enhancements

--- eval/tools/bpipe/0.9.9.3/templates/executor/sge-command.template.sh
+++ eval/tools/bpipe/0.9.9.3/templates/executor/sge-command.template.sh
@@ -5,50 +5,20 @@
 #\$ -o $jobDir/$CMD_OUT_FILENAME
 #\$ -e $jobDir/$CMD_ERR_FILENAME
 #\$ -notify
-<%if(config?.walltime) {%>#\$ -l h_rt=${config.walltime} <%}
-procs=null
-if(config.containsKey('procs') && config.procs.toString().isInteger()) {
-    procs = config.procs.toString().toInteger()
-}
-if(procs>1) {
-    if(config.containsKey('sge_pe')) {%>
-#\$ -pe ${config.sge_pe} ${config.procs} <%
-    }
-    else {%>
-#\$ -l slots=${config.procs} <%
-    }
-}
-if(config?.memory) {
-    mem_param = 'virtual_free'
-    if(config.containsKey('mem_param')) {
-        mem_param = config.mem_param 
-    } 
-    if(procs>1 && config.containsKey('smp_mem_param' )) {
-        mem_param = config.smp_mem_param 
-    }
-}%>
-#\$ -l ${mem_param}=${config.memory}
-
-# Executed usinq queue ${config.queue}
-
-function signal_handler() {
-    echo 255 > $jobDir/$CMD_EXIT_FILENAME;
-}
-
-export signal_handler
-
-trap signal_handler HUP INT TERM QUIT KILL STOP USR1 USR2
-
-<%if(config.containsKey('use')) {%>
-<%if(config.containsKey('use_prefix')) {%> ${config.use_prefix} <%}%>
-use ${config.use}
+<%if(config?.walltime) {%>#\$ -l h_rt=${config.walltime} <%}%>
+<%if(config?.sge_pe && config?.procs) {%>
+#\$ -pe ${config.sge_pe} ${config.procs} <%}
+if(config?.procs && config.procs.toString().isInteger() && !config.containsKey("sge_pe") && config.procs.toString().toInteger()>1 ) { %>
+#\$ -l slots=${config.procs} <%}
+if(config?.memory) {%>
+#\$ -l virtual_free=${config.memory}
 <%}%>
+
+trap 'echo 255 > $jobDir/$CMD_EXIT_FILENAME;' HUP INT TERM QUIT KILL STOP USR1 USR2
 
 (
 bash -e ${CMD_FILENAME.absolutePath}
-) &
-
-wait \$!
+)
 
 result=\$?
 echo -n \$result > $jobDir/$CMD_EXIT_FILENAME
