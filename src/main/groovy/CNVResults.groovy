4833333153d193795de22f6f2a500e56323feb5c
Simon Sadedin <ssadedin@gmail.com>
2017-06-03 18:31:49 +1000

    measure freq of ANY kind of overlapping CNV rather than matching type

--- src/main/groovy/CNVResults.groovy
+++ src/main/groovy/CNVResults.groovy
@@ -35,7 +35,11 @@ abstract class CNVResults extends RangedData {
                     ]
                     
                     if(annotator) {
-                        row.spanningFreq = annotator.annotate(cnv, cnv.type).spanningFreq
+                        // Used to match the CNV type here, but after seeing that some
+                        // CNV callers get confused and call a differentn type in a region where there is 
+                        // a CNV,  I now think it's more accurate to return the frequency of ALL cnvs
+                        // overlapping
+                        row.spanningFreq = annotator.annotate(cnv, "ANY" /* cnv.type */).spanningFreq
                         row += annotator.annotateSize(cnv)
                     }
                     
