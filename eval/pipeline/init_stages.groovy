ee18c46793ce62f1f5402b0fc838040b9fe09187
Simon Sadedin <ssadedin@gmail.com>
2017-07-09 07:21:28 +1000

    add / fix configs for smallish jobs

--- eval/pipeline/init_stages.groovy
+++ eval/pipeline/init_stages.groovy
@@ -45,7 +45,7 @@ create_analysable_target = {
 
            
            filteredTargets.save("$output.bed", sorted:true)
-        ""","small"
+        """,config:"small"
    }
    branch.analysable_target = output.bed
 }
