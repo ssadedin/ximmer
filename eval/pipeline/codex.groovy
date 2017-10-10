c85e42461fc77aa8f06cb09c2cccd6fbbb877e1c
Simon Sadedin <ssadedin@gmail.com>
2017-10-11 10:04:23 +1100

    fix hard coded output and single sample calling for CODEX

--- eval/pipeline/codex.groovy
+++ eval/pipeline/codex.groovy
@@ -32,55 +32,58 @@ codex_call_cnvs = {
                 return(read.group.info$SM[[2]])
             })))
 
-            bedFile <- "$input.bed"
-            chr <- "$chr"
-
-            bambedObj <- getbambed(bamdir = "$bamDir", bedFile = bedFile,
-                                     sampname = sampname, projectname = "sureselect_sim", chr)
-            
-            bamdir <- bambedObj$bamdir; sampname <- bambedObj$sampname
-            
-            
-            ref <- bambedObj$ref; 
-            projectname <- bambedObj$projectname; 
-            chr <- bambedObj$chr
-            
-            
-            coverageObj <- getcoverage(bambedObj, mapqthres = 20)
-            
-            Y <- coverageObj$Y; readlength <- coverageObj$readlength
-            
-            gc <- getgc(chr, ref)
-            
-            mapp <- getmapp(chr, ref)
-            
-            qcObj <- qc(Y, sampname, chr, ref, mapp, gc, cov_thresh = c(20, 4000),
-                        length_thresh = c(20, 2000), mapp_thresh = 0.9, gc_thresh = c(20, 80))
-            
-            Y_qc <- qcObj$Y_qc; 
-            sampname_qc <- qcObj$sampname_qc; 
-            gc_qc <- qcObj$gc_qc
-            
-            mapp_qc <- qcObj$mapp_qc; ref_qc <- qcObj$ref_qc; qcmat <- qcObj$qcmat
-            
-            normObj <- normalize(Y_qc, gc_qc, K = 1:9)
-            
-            Yhat <- normObj$Yhat; AIC <- normObj$AIC; BIC <- normObj$BIC
-
-            RSS <- normObj$RSS; K <- normObj$K
-            
-            optK = K[which.max(BIC)]
-            
-            finalcall <- segment(Y_qc, Yhat, optK = optK, K = K, sampname_qc,
-                                 ref_qc, chr, lmax = 200, mode = "integer")
-
-            write.table(finalcall,
-                        file="/misc/vcgs/exome/ximmer/codex.tsv",
-                        row.names=F,
-                        col.names=T,
-                        quote=F,
-                        sep="\\t"
-                        )
+            for(bam_sample in bam.samples) {
+                bedFile <- "$input.bed"
+                chr <- "$chr"
+    
+                bambedObj <- getbambed(bamdir = "$bamDir", bedFile = bedFile,
+                                         sampname = bam_sample, projectname = "sureselect_sim", chr)
+                
+                bamdir <- bambedObj$bamdir; sampname <- bambedObj$sampname
+                
+                
+                ref <- bambedObj$ref; 
+                projectname <- bambedObj$projectname; 
+                chr <- bambedObj$chr
+                
+                
+                coverageObj <- getcoverage(bambedObj, mapqthres = 20)
+                
+                Y <- coverageObj$Y; readlength <- coverageObj$readlength
+                
+                gc <- getgc(chr, ref)
+                
+                mapp <- getmapp(chr, ref)
+                
+                qcObj <- qc(Y, sampname, chr, ref, mapp, gc, cov_thresh = c(20, 4000),
+                            length_thresh = c(20, 2000), mapp_thresh = 0.9, gc_thresh = c(20, 80))
+                
+                Y_qc <- qcObj$Y_qc; 
+                sampname_qc <- qcObj$sampname_qc; 
+                gc_qc <- qcObj$gc_qc
+                
+                mapp_qc <- qcObj$mapp_qc; ref_qc <- qcObj$ref_qc; qcmat <- qcObj$qcmat
+                
+                normObj <- normalize(Y_qc, gc_qc, K = 1:9)
+                
+                Yhat <- normObj$Yhat; AIC <- normObj$AIC; BIC <- normObj$BIC
+    
+                RSS <- normObj$RSS; K <- normObj$K
+                
+                optK = K[which.max(BIC)]
+                
+                finalcall <- segment(Y_qc, Yhat, optK = optK, K = K, sampname_qc,
+                                     ref_qc, chr, lmax = 200, mode = "integer")
+    
+                write.table(finalcall,
+                            file="$output.tsv",
+                            row.names=F,
+                            col.names=T,
+                            quote=F,
+                            append=T,
+                            sep="\\t"
+                            )
+            }
         """}, "codex")
     }
     branch.caller_result = output.tsv
