850196b265a07faa151a29ed30edb2eb69d9c1bf
Simon Sadedin <ssadedin@gmail.com>
2019-03-05 10:41:41 +1100

    added delly result parser

---
+++ src/test/groovy/ximmer/results/DellyResultsTest.groovy
@@ -0,0 +1,34 @@
+package ximmer.results
+
+import static org.junit.Assert.*
+
+import gngs.Region
+import org.junit.Test
+
+class DellyResultsTest {
+
+    @Test
+    public void test() {
+        DellyResults dr = new DellyResults('src/test/data/test.delly.vcf')
+        Region dup = new Region("chr1:10200-10443")
+        
+        assert dr.getOverlaps(dup).size() == 1
+        
+        Region dellyDup = dr.find { it.overlaps(dup) }
+        assert dellyDup.type == 'DUP'
+        assert dellyDup.quality > 10
+        
+    }
+    
+    @Test
+    void 'test no inversions'() {
+        DellyResults dr = new DellyResults('src/test/data/test.delly.vcf')
+        
+        assert !dr*.type.any { it.contains('INV') }
+        
+        Region inv = new Region("chr1:782758-782858")
+        
+        assert dr.getOverlaps(inv).isEmpty()
+    }
+    
+}
