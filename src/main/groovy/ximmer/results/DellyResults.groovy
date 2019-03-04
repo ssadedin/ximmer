27bd721beb655def9b9b8e16c9d39a676e1199a9
Simon Sadedin <ssadedin@gmail.com>
2019-03-05 10:41:41 +1100

    added delly result parser

---
+++ src/main/groovy/ximmer/results/DellyResults.groovy
@@ -0,0 +1,30 @@
+package ximmer.results
+
+import gngs.*
+
+class DellyResults extends CNVResults {
+    
+    String sourceFile
+    String sample
+    
+    public DellyResults(String sourceFile, String sample=null) {
+		super(sourceFile)
+        this.sourceFile = sourceFile
+		this.sample = sample ? sample : getSampleFromFile(sourceFile)
+		
+		def v = VCF.parse(this.sourceFile) {
+            if(!(it.info.SVTYPE in ['DEL','DUP']))
+                return
+                
+			Region r = new Region(it.chr, it.pos, it.pos + it.info.END.toInteger())
+			r.type = it.info.SVTYPE
+			r.sample = this.sample
+			r.start = it.pos
+			r.end = it.pos + it.info.END.toInteger()
+			r.quality = it.filter == 'PASS' ? 20 : 0
+			addRegion(r)
+		}
+    }
+	
+}
+
