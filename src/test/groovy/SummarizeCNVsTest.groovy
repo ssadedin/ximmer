128b0b2a7c1789cfcea400d5984127ae3daeb095
Simon Sadedin <ssadedin@gmail.com>
2019-03-05 21:46:42 +1100

    add individual call spans to JSON output
    note: also renamed cnvnator to cnvn

--- src/test/groovy/SummarizeCNVsTest.groovy
+++ src/test/groovy/SummarizeCNVsTest.groovy
@@ -11,13 +11,38 @@ class SummarizeCNVsTest {
 	
 	Map<String,RangedData> results = [:]
 	
+    SummarizeCNVs scnvs = new SummarizeCNVs()
+    
+    Region cnv = new Region('chr1:1000-2000')
+    
 	@Test
-	public void 'Test parseCallerOpts Default Behaviour'() {
-		// Figure out how to test this 
-		/*
-		List<String> defaultOps = []
-		parseCallerOpt("ed", defaultOps, { new ExomeDepthResults(it) }, results)
-		*/
+	public void 'basic CNV annotation'() {
+        cnv.sample = 'FOO'
+        
+        Map data = scnvs.cnvToMap([], [], SummarizeCNVs.DEFAULT_JS_COLUMNS + [], cnv)
+           
+        Utils.table([data])
+        
+        assert data.sample == 'FOO'
 	}
-
+    
+    @Test
+    void 'test caller span annotations'() {
+        List callers = ['ed','xhmm']
+        
+        Region ed = new Region('chr1:900-1300')
+        ed.quality = 10
+        ed.calls = [ed]
+        cnv.ed = ed
+        
+        Map data = scnvs.cnvToMap(callers, [], scnvs.computeColumns(callers,[]), cnv)
+        
+        println data
+        
+        Utils.table([data])
+        
+        assert data.ed == 'TRUE'
+        assert data.xhmm == 'FALSE'
+        assert data.calls.ed[0] == [900,1300,10]
+    }
 }
