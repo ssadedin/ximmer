e6841b9e5577e21d4d3469f2e84bbc2e01dec84c
Simon Sadedin <ssadedin@gmail.com>
2019-03-05 22:41:27 +1100

    add delly support to SummarizeCNVs

--- src/main/groovy/ximmer/results/DellyResults.groovy
+++ src/main/groovy/ximmer/results/DellyResults.groovy
@@ -12,17 +12,21 @@ class DellyResults extends CNVResults {
         this.sourceFile = sourceFile
 		this.sample = sample ? sample : getSampleFromFile(sourceFile)
 		
-		def v = VCF.parse(this.sourceFile) {
-            if(!(it.info.SVTYPE in ['DEL','DUP']))
+		VCF.parse(this.sourceFile) {
+            if(!(it.info.SVTYPE in ['DEL','DUP'])) {
                 return
+            }
                 
-			Region r = new Region(it.chr, it.pos, it.pos + it.info.END.toInteger())
+			Region r = new Region(it.chr, it.pos, it.info.END.toInteger())
 			r.type = it.info.SVTYPE
 			r.sample = this.sample
 			r.start = it.pos
-			r.end = it.pos + it.info.END.toInteger()
+			r.end = it.info.END.toInteger()
 			r.quality = it.filter == 'PASS' ? 20 : 0
+            
 			addRegion(r)
+            
+            return false
 		}
     }
 	
