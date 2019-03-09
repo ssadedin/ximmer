22a9fce3100a4e2d124283f2227d1776c923d8f1
Simon Sadedin <ssadedin@gmail.com>
2019-03-09 23:57:15 +1100

    fix lumpy results test

--- src/test/groovy/ximmer/results/LumpyResultsTest.groovy
+++ src/test/groovy/ximmer/results/LumpyResultsTest.groovy
@@ -12,14 +12,15 @@ class LumpyResultsTest {
     @Test
     public void test() {
         String vcf_path = 'src/test/data/test.small.lumpy.vcf'
-        LumpyResults lr = new LumpyResults('src/test/data/test.lumpy.vcf')
-        Region dup = new Region("chrM:7283-7743")
+        LumpyResults lr = new LumpyResults('src/test/data/test.small.lumpy.vcf')
+        Region dup = new Region("chr2:7283-7743")
 
         assert lr.getOverlaps(dup).size() == 1
 
         Region lumpyDup = lr.find { it.overlaps(dup) }
         assert lumpyDup.type == 'DUP'
         assert lumpyDup.quality > 9
+        assert lumpyDup.size() == 368
 
     }
 
