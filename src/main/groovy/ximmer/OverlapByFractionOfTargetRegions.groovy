39aafcb84b6b66f2ee7cd9f970d94dcfbbb823ca
Simon Sadedin <ssadedin@gmail.com>
2021-05-28 17:25:47 +1000

    fix overlap by shared targets not working

--- src/main/groovy/ximmer/OverlapByFractionOfTargetRegions.groovy
+++ src/main/groovy/ximmer/OverlapByFractionOfTargetRegions.groovy
@@ -17,8 +17,7 @@ class OverlapByFractionOfTargetRegions implements OverlapCriteria {
         List<Region> r1Overlaps = targetRegions.getOverlapRegions(r1)
         List<Region> r2Overlaps = targetRegions.getOverlapRegions(r2)
         
-        int mutual = (int)r1Overlaps.count { Region r -> r in r2Overlaps }
-        
+        int mutual = (int)r1Overlaps.count { Region r -> r2Overlaps.any { r.overlaps(it) } }
         int total = (
             r1Overlaps.size() + r2Overlaps.size()
             - mutual /* the mutual ones will have been counted twice otherwise */)
