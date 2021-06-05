3409d20ca7922a8475c25ae190c93a425b43d6a5
Simon Sadedin <ssadedin@gmail.com>
2021-06-05 11:35:49 +1000

    add missing file defining delfin stages

---
+++ eval/pipeline/delfin_stages.groovy
@@ -0,0 +1,25 @@
+
+delfin = {
+    
+    requires DELFIN_CONTROL_DIR : "Directory containing interval files of samples to use as controls",
+             sample_names : "The names of the samples to analyse"
+    
+    output.dir = "delfin"
+    
+    def cov_files = 
+        file(DELFIN_CONTROL_DIR)
+            .listFiles()
+            .findAll { it.name.endsWith('interval_summary') } 
+
+    produce(batch_name + '.delfin.cnvs.tsv') {
+        exec """
+            $JAVA -Xmx${memory}g -cp $GROOVY_ALL_JAR:$GNGS_JAR gngs.tools.Delfin
+                -t $input.bed ${sample_names.collect { "-s $it"}.join(" ")}
+                -o $output.cnvs.tsv
+                -lr $output.cnvs.tsv.prefix ${cov_files.collect { "-cov $it"}.join(' ')}
+        """, "delfin"
+    }
+    
+    
+    branch.caller_result = output.tsv
+}
