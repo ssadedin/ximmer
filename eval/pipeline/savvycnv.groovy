fc66eaa7a7e23f2cf0c93918654a290de0be00b0
Simon Sadedin <simon.sadedin@mcri.edu.au>
2022-04-17 12:14:35 +1000

    change savvy to work with gradle built version, better validation

--- eval/pipeline/savvycnv.groovy
+++ eval/pipeline/savvycnv.groovy
@@ -2,21 +2,32 @@
 // Implementation of SavvyCNV pipeline
 //
 init_savvycnv = {
+    
+    final String CONFIG_FILE = "eval/pipeline/config.groovy"
+    
+    requires SAVVYCNV_HOME : 'The path to SavvyCNV. If you are running this pipeline from Ximmer, ensure the location is configured in $CONFIG_FILE'
+    
+    if(!file(SAVVYCNV_HOME).exists())
+        throw new bpipe.PipelineError("SavvyCNV location $SAVVYCNV_HOME was not found.  Please check this in $CONFIG_FILE")
+
+    branch.SAVVYCNV_JAR="$SAVVYCNV_HOME/build/libs/SavvySuite-all.jar"
+
+    if(!file(SAVVYCNV_JAR).exists())
+        throw new bpipe.PipelineError("SavvyCNV jar file $SAVVYCNV_JAR was not found.  Please check SavvySuite was built successfully")
+
     branch.dir = "analysis/savvy"
 }
 
 savvy_bin_coverage = {
 
-    requires GATK4_JAR : 'Location of jar file for GATK'
-
     def sample = new gngs.SAM(input.bam.toString()).samples[0]
 
     produce(sample + '.coverageBinner') {
         exec """
-            export CLASSPATH="$GATK4_JAR:$SAVVYCNV_HOME"
+            export CLASSPATH="$SAVVYCNV_JAR"
 
             $JAVA -Xmx1g CoverageBinner $input.bam > $output.coverageBinner
-        """
+        ""","savvycnv_coverage"
     }
 }
 
@@ -27,12 +38,12 @@ savvy_call_cnvs = {
     
     produce('savvy.cnvs.tsv') {
         exec """
-            export CLASSPATH="$GATK4_JAR:$SAVVYCNV_HOME"
+            export CLASSPATH="$SAVVYCNV_JAR"
 
             printf "chromosome\\tstart\\tend\\ttype\\tblock_support\\tblock_span\\t\\tqual\\tqual_rel\\tsample\\n" > $output.tsv
 
-            $JAVA -Xmx30g SavvyCNV -data -d $savvy_chunk_size -trans $savvy_transition_probability $inputs.coverageBinner >> $output.tsv 
-        """
+            $JAVA -Xmx${memory}g SavvyCNV -data -d $savvy_chunk_size -trans $savvy_transition_probability $inputs.coverageBinner >> $output.tsv 
+        ""","savvycnv"
     }
 
     branch.caller_result = output.tsv
@@ -41,10 +52,3 @@ savvy_call_cnvs = {
 savvy_cnv = segment {
     init_savvycnv + '%.bam' * [ savvy_bin_coverage ] + savvy_call_cnvs
 }
-
-
-
-
-
-
-
