e02d59a098879b44e031e32872fb4e34ae6b662f
Simon Sadedin <ssadedin@gmail.com>
2023-05-31 14:32:38 +1000

    add support for schism when merging / summarizing CNVs

--- src/main/groovy/ximmer/OverlapBySpan.groovy
+++ src/main/groovy/ximmer/OverlapBySpan.groovy
@@ -10,6 +10,12 @@ class OverlapBySpan implements OverlapCriteria {
     
     @Override
     public boolean overlaps(Region r1, Region r2) {
+        final int maxBreakEndDistance = 3i;
+        
+        if(r1['type'] == 'BND' || r2['type'] == 'BND') {
+            return Math.abs(r1.from - r2.from) < maxBreakEndDistance || Math.abs(r2.to - r1.to) < maxBreakEndDistance
+        }
+
         return r1.mutualOverlap(r2) > minimumFraction;
     }
 }
