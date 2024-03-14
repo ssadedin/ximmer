072c32a6b51720b0f84c335f246cd8d282c17774
Simon Sadedin <ssadedin@gmail.com>
2024-03-14 18:28:09 +1100

    add support for raw cxp post to post file contents

--- src/main/groovy/RawPostToCXP.groovy
+++ src/main/groovy/RawPostToCXP.groovy
@@ -3,6 +3,7 @@ import java.util.regex.Pattern
 import com.github.scribejava.core.builder.api.DefaultApi10a
 
 import gngs.*
+import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 import groovy.util.logging.Log
 import htsjdk.samtools.SAMRecord
@@ -35,15 +36,17 @@ class RawPostToCXP extends ToolBase {
         ws.autoSlash = false
         ws.credentialsPath = ".cxp/credentials"
         
-        def data = new JsonSlurper().parse(System.in)
+        def data =  new JsonSlurper().parseText(opts.arguments() ? new File(opts.arguments()[0]).text : System.in.text)
         
         log.info 'Posting data: ' + data
         
-        ws.post(data)
+        def result = ws.post(data)
+        
+        log.info "POST succeeded, result: \n" + new JsonBuilder(result).toPrettyString()
     }
     
     static void main(String [] args) {
-        cli('RawPostToCXP -url <full post url>', args) {
+        cli('RawPostToCXP -url <full post url> [file]', args) {
             url 'URL to post to', args:1, required: true
         }
     }
