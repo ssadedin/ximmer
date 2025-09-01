deb5815d35aa06115882ba653a8f137145e3529b
Simon Sadedin <simon.sadedin@mcri.edu.au>
2025-09-01 08:18:15 +0000

    feat: Add TSVtoVCFTest Groovy test file

---
+++ src/test/groovy/ximmer/TSVtoVCFTest.groovy
@@ -0,0 +1,20 @@
+package ximmer
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+class TSVtoVCFTest {
+    // no dot in CR
+    // no N as REF
+
+    /**
+     * Test the createVariantFromLine method
+     * Single deletion on input maps to single deletion in output variant context
+     */
+    @Test
+    public void testSimpleDeletion() {
+        fail("Not yet implemented")
+    }
+
+}
